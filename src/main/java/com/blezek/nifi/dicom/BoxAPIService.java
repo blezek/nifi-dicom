75d3a03cdd59ac704e724cad5a031d6344b3cc72
Daniel Blezek <blezek.daniel@mayo.edu>
2018-10-24 13:33:23 -0500

    Remove Box code

--- src/main/java/com/blezek/nifi/dicom/BoxAPIService.java
+++
@@ -1,9 +0,0 @@
-package com.blezek.nifi.dicom;
-
-import com.box.sdk.BoxAPIConnection;
-
-import org.apache.nifi.controller.ControllerService;
-
-public interface BoxAPIService extends ControllerService {
-  BoxAPIConnection getConnection();
-}
